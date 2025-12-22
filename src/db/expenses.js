74c6dd8917001899853932f1c50325ada9992d89
merosman91 <merghanigasimosman@gmail.com>
2025-12-22 20:10:21 +0200

    Update expenses.js

--- src/db/expenses.js
+++ src/db/expenses.js
@@ -1,17 +1,8 @@
 import { openDB } from "./db"
 
-export async function addExpense(expense) {
+export async function addExpense(e) {
   const db = await openDB()
-  const tx = db.transaction("expenses", "readwrite")
-  tx.objectStore("expenses").put(expense)
+  db.transaction("expenses", "readwrite")
+    .objectStore("expenses")
+    .put(e)
 }
-
-export async function getExpensesByCycle(cycleId) {
-  const db = await openDB()
-  return new Promise(res => {
-    const tx = db.transaction("expenses", "readonly")
-    const req = tx.objectStore("expenses").getAll()
-    req.onsuccess = () =>
-      res(req.result.filter(e => e.cycleId === cycleId))
-  })
-      }
