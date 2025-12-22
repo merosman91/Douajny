73c2e617615228af41cb3365aa457700438e47a5
merosman91 <merghanigasimosman@gmail.com>
2025-12-22 20:06:11 +0200

    Update inventory.js

--- src/db/inventory.js
+++ src/db/inventory.js
@@ -1,17 +1,27 @@
 import { openDB } from "./db"
 
-export async function addInventory(inventory) {
+export async function addItem(item) {
   const db = await openDB()
   const tx = db.transaction("inventory", "readwrite")
-  tx.objectStore("inventory").put(inventory)
+  tx.objectStore("inventory").put(item)
 }
 
 export async function getInventoryByCycle(cycleId) {
   const db = await openDB()
   return new Promise(res => {
     const tx = db.transaction("inventory", "readonly")
-    const req = tx.objectStore("inventory").getAll()
-    req.onsuccess = () =>
-      res(req.result.filter(e => e.cycleId === cycleId))
+    tx.objectStore("inventory").getAll().onsuccess = e =>
+      res(e.target.result.filter(i => i.cycleId === cycleId))
   })
+}
+
+export async function consumeItem(id, qty) {
+  const db = await openDB()
+  const tx = db.transaction("inventory", "readwrite")
+  const store = tx.objectStore("inventory")
+  store.get(id).onsuccess = e => {
+    const item = e.target.result
+    item.quantity -= qty
+    store.put(item)
+  }
 }
