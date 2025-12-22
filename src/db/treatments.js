ebdf110334329f46f009f58dc6c5a7f17d40d43c
merosman91 <merghanigasimosman@gmail.com>
2025-12-22 20:07:14 +0200

    Update treatments.js

--- src/db/treatments.js
+++ src/db/treatments.js
@@ -1,27 +1,11 @@
 import { openDB } from "./db"
+import { consumeItem } from "./inventory"
 
-export const initTreatmentsStore = db => {
-  if (!db.objectStoreNames.contains("treatments")) {
-    const store = db.createObjectStore("treatments", {
-      keyPath: "id",
-      autoIncrement: true
-    })
-    store.createIndex("cycleId", "cycleId", { unique: false })
-  }
-}
-
-export const addTreatment = async record => {
+export async function addTreatment(t) {
   const db = await openDB()
   const tx = db.transaction("treatments", "readwrite")
-  tx.objectStore("treatments").add(record)
-}
+  tx.objectStore("treatments").put(t)
 
-export const getTreatmentsByCycle = async cycleId => {
-  const db = await openDB()
-  return new Promise(resolve => {
-    const tx = db.transaction("treatments", "readonly")
-    const store = tx.objectStore("treatments")
-    const index = store.index("cycleId")
-    index.getAll(cycleId).onsuccess = e => resolve(e.target.result)
-  })
-      }
+  // خصم من المخزون
+  await consumeItem(t.inventoryId, t.usedQty)
+}
