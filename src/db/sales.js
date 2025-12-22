8ba207b94272afb443bcf729f5d9a32f3a1fbadf
merosman91 <merghanigasimosman@gmail.com>
2025-12-22 20:11:12 +0200

    Update sales.js

--- src/db/sales.js
+++ src/db/sales.js
@@ -1,17 +1,8 @@
 import { openDB } from "./db"
 
-export async function addSales(sales) {
+export async function addSale(s) {
   const db = await openDB()
-  const tx = db.transaction("sales", "readwrite")
-  tx.objectStore("sales").put(sales)
-}
-
-export async function getSalesByCycle(cycleId) {
-  const db = await openDB()
-  return new Promise(res => {
-    const tx = db.transaction("sales", "readonly")
-    const req = tx.objectStore("sales").getAll()
-    req.onsuccess = () =>
-      res(req.result.filter(e => e.cycleId === cycleId))
-  })
+  db.transaction("sales", "readwrite")
+    .objectStore("sales")
+    .put(s)
 }
